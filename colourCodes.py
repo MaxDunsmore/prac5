5ab16f383857ecc700ddfc487ace5162d26c258a
MaxDunsmore <max.dunsmore@my.jcu.edu.au>
2017-03-23 12:51:59 +1000

    prac4

--- colourCodes.py
+++ colourCodes.py
@@ -1,6 +1,6 @@
 """
 CP1404 Practical
-Colour codes in a dictionary
+Colour code in a dictionary
 """
 
 colour_codes = {"alice blue": "f0f8ff", "beige": "f5fdc", "black": "000000", "aquamarine": "7fffdf", "brown": "a52a2a", "Cadet Blue": "5f9ea0", "chocolate": "d2691e"}
